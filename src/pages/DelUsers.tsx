adddf73cd5fa671bb6ab31ca4a85db0d2bd04242
HeyTiainnes <etienne.pomente@reseau.sncf.fr>
2023-02-27 17:56:22 +0100

    del ok? a tester ....

--- src/pages/DelUsers.tsx
+++ src/pages/DelUsers.tsx
@@ -1,9 +1,28 @@
-import { useRef, useState } from "react";
+import { useRef, useEffect } from "react";
 import axios from "axios";
+import { useState } from "react";
 
-const DelUsers = () => {
+export interface UsersList {
+    id_users: string;
+    name: string;
+    mail: string;
+    password: string;
+}
 
+let supp: UsersList[] = [];
 
+const DelUsers = (UserAsupprimer: UsersList) => {
+    const [listUsers, setlistUsers] = useState<UsersList[]>([]);
+
+    axios
+        .delete(`http://localhost:3000/theyUsers/${UserAsupprimer.id_users}`)
+
+        .then((retourUserAsupprimer) => {
+            supp = retourUserAsupprimer.data;
+            window.location.reload();
+        })
+
+    setlistUsers(supp);
 
 }
 
