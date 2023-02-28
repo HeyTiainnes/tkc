b851a58a65a035921b077ee7294b1158655489df
HeyTiainnes <etienne.pomente@reseau.sncf.fr>
2023-02-28 14:52:10 +0100

    debut NewTask

--- src/pages/NewTask.tsx
+++ src/pages/NewTask.tsx
@@ -1,49 +1,45 @@
-import axios from "axios";
-import "./NewTask.css";
-
-
+import React from "react";
 
 const NewTask = () => {
-
-
-
     return (
-        <div className="container">
-            <form>
-                <div className="form-group">
-                    <label htmlFor="exampleFormControlInput1">Email address</label><br></br>
-                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="exampleFormControlSelect1">Example select</label><br></br>
-                    <select className="form-control" id="exampleFormControlSelect1">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                    </select>
-                </div>
-                {/* <div className="form-group">
-                    <label htmlFor="exampleFormControlSelect2">Example multiple select</label>
-                    <select multiple className="form-control" id="exampleFormControlSelect2">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                    </select>
-                </div> */}
-                <div className="form-group">
-                    <label htmlFor="exampleFormControlTextarea1">Example textarea</label><br></br>
-                    <textarea className="form-control" id="exampleFormControlTextarea1" >
+        <>
 
-                    </textarea>
-                </div>
-            </form>
-        </div>
+            <div className="container">
+                <form>
+                    <label htmlFor="designation">
+                        Designation<span className="required">*</span>
+                    </label>
+                    <input type="text" id="designation" name="designation" required />
+
+                    <label htmlFor="deadline">Deadline</label>
+                    <input type="datetime-local" id="deadline" name="deadline" />
+
+                    <label htmlFor="duration">Duration</label>
+                    <input
+                        type="text"
+                        id="duration"
+                        name="duration"
+                        placeholder="eg. 2d 3h 30m"
+                    />
+
+                    <label htmlFor="startdate">Start Date</label>
+                    <input type="datetime-local" id="startdate" name="startdate" />
+
+                    <label htmlFor="notes">Notes (255 characters max)</label>
+                    <textarea id="notes" name="notes" maxLength={255}></textarea>
+
+                    <div className="buttons">
+                        <button type="reset" id="cancel">
+                            Cancel
+                        </button>
+                        <button type="submit" id="submit">
+                            Validate
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </>
     );
 };
-
 
 export default NewTask;
