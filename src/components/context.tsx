c22b5e4c1c6430b2dbf58769673ee0f099f78f85
HeyTiainnes <etienne.pomente@reseau.sncf.fr>
2023-06-15 15:21:43 +0200

    ok by user create

---
+++ src/components/context.tsx
@@ -0,0 +1,18 @@
+// import jwt, { JwtPayload } from 'jsonwebtoken';
+
+// const token = localStorage.getItem('accessToken');
+
+// if (token) {
+//     const decodedToken = jwt.verify(token, 'krakoukass') as JwtPayload;
+//     const userEmail = decodedToken?.mail;
+//     if (userEmail) {
+//         const user = getUserByEmail(userEmail);
+//         const userMail = user.mail;
+//         // Utilisez l'e-mail de l'utilisateur dans votre application
+//     }
+// }
+// function getUserByEmail(userEmail: string) {
+//     throw new Error('Function not implemented.');
+// }
+
+export { }
