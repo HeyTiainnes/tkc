5e7e52bd462121149650f8a6684cbb46310619fe
HeyTiainnes <etienne.pomente@reseau.sncf.fr>
2023-06-24 19:25:47 +0200

    new CLI ss tskID

--- src/pages/NewCheckList.tsx
+++ src/pages/NewCheckList.tsx
@@ -1,22 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-const CheckList = () => {
-    const checkListItems = ["Item 1", "Item 2", "Item 3"];
+const NewCheckList = () => {
+    const [name, setName] = useState("");
+    const [notes, setNotes] = useState("");
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        try {
+            // Envoyer les données à la base de données via Axios
+            const response = await axios.post("http://localhost:3000/checkListItems", {
+                name,
+                notes,
+            });
+
+            // Réinitialiser les champs du formulaire après l'enregistrement réussi
+            setName("");
+            setNotes("");
+
+            // Traiter la réponse ou effectuer d'autres actions nécessaires
+            console.log(response.data);
+        } catch (error) {
+            // Gérer les erreurs de manière appropriée
+            console.error(error);
+        }
+    };
 
     return (
         <div>
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <p>Lorem250</p>
-            <br />
-            {checkListItems.map((item, index) => (
-                <p key={index}>{item}</p>
-            ))}
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Nom de la sous-tâche:</label>
+                <input
+                    type="text"
+                    id="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+
+                <label htmlFor="notes">Notes (max. 1024 caractères):</label>
+                <textarea
+                    id="notes"
+                    value={notes}
+                    onChange={(e) => setNotes(e.target.value)}
+                />
+
+                <button type="submit">Enregistrer</button>
+            </form>
         </div>
     );
 };
 
-export default CheckList;
+export default NewCheckList;
