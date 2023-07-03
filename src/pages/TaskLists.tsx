bf4838f93f896af38581fcaf04296d2e2b2d98b1
HeyTiainnes <etienne.pomente@reseau.sncf.fr>
2023-07-03 21:05:31 +0200

    rectif err 500

--- src/pages/TaskLists.tsx
+++ src/pages/TaskLists.tsx
@@ -1,3 +1,158 @@
+// import React, { useState, useEffect } from 'react';
+// import { Link, useNavigate } from 'react-router-dom';
+// import axios from 'axios';
+
+// export type Task = {
+//     id: number;
+//     designation: string;
+//     deadline: string;
+//     notes: string;
+//     done: boolean;
+//     checkListItems?: CheckListItem[];
+// };
+
+// export type CheckListItem = {
+//     id: number;
+//     name: string;
+// };
+
+// const TaskLists = () => {
+//     const [tasks, setTasks] = useState<Task[]>([]);
+//     const navigate = useNavigate();
+
+//     useEffect(() => {
+//         const fetchTasks = async () => {
+//             try {
+//                 const response = await axios.get('http://localhost:3000/tasks', {
+//                     headers: {
+//                         Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+//                     },
+//                 });
+//                 setTasks(response.data);
+//             } catch (error) {
+//                 console.error(error);
+//             }
+//         };
+
+//         fetchTasks();
+//     }, []);
+
+//     const fetchCheckListItems = async (tasksId: number) => {
+//         try {
+//             const response = await axios.get(
+//                 `http://localhost:3000/checkListItems?tasksId=${tasksId}`,
+//                 {
+//                     headers: {
+//                         Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+//                     },
+//                 }
+//             );
+//             const updatedTasks = tasks.map((task) => {
+//                 if (task.id === tasksId) {
+//                     return { ...task, checkListItems: response.data };
+//                 }
+//                 return task;
+//             });
+//             setTasks(updatedTasks);
+//         } catch (error) {
+//             console.error(error);
+//         }
+//     };
+
+//     const handleDone = (taskId: number) => {
+//         const updatedTasks = tasks.map((task) => {
+//             if (task.id === taskId) {
+//                 return { ...task, done: !task.done };
+//             }
+//             return task;
+//         });
+
+//         setTasks(updatedTasks);
+//     };
+
+//     const handleDelete = (taskId: number) => {
+//         axios
+//             .delete(`http://localhost:3000/tasks/${taskId}`, {
+//                 headers: {
+//                     Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+//                 },
+//             })
+//             .then(() => {
+//                 const updatedTasks = tasks.filter((task) => task.id !== taskId);
+//                 setTasks(updatedTasks);
+//             })
+//             .catch((error) => {
+//                 console.error(error);
+//             });
+//     };
+
+//     const handleAddCheckListItem = (taskId: number) => {
+//         navigate(`/NewCheckList/${taskId}`);
+//     };
+
+//     const handleDoneCheckListItem = (taskId: number, checkListItemId: number) => {
+//         const updatedTasks = tasks.map((task) => {
+//             if (task.id === taskId && task.checkListItems) {
+//                 const updatedCheckListItems = task.checkListItems.filter(
+//                     (item) => item.id !== checkListItemId
+//                 );
+//                 return { ...task, checkListItems: updatedCheckListItems };
+//             }
+//             return task;
+//         });
+
+//         setTasks(updatedTasks);
+//     };
+
+//     useEffect(() => {
+//         tasks.forEach((task) => {
+//             if (!task.checkListItems) {
+//                 fetchCheckListItems(task.id);
+//             }
+//         });
+//     }, [tasks]);
+
+//     return (
+//         <div>
+//             <h1>Task List</h1>
+//             <ul>
+//                 {tasks.map((task) => (
+//                     <li key={task.id}>
+//                         <input
+//                             type="checkbox"
+//                             checked={task.done}
+//                             onChange={() => handleDone(task.id)}
+//                         />
+//                         <span>{task.designation}</span>
+//                         <button onClick={() => handleDelete(task.id)}>Delete</button>
+//                         <button onClick={() => handleAddCheckListItem(task.id)}>
+//                             Add Check List Item
+//                         </button>
+//                         {task.checkListItems && (
+//                             <ul>
+//                                 {task.checkListItems.map((checkListItem) => (
+//                                     <li key={checkListItem.id}>
+//                                         {checkListItem.name}
+//                                         <button
+//                                             onClick={() =>
+//                                                 handleDoneCheckListItem(task.id, checkListItem.id)
+//                                             }
+//                                         >
+//                                             Done
+//                                         </button>
+//                                     </li>
+//                                 ))}
+//                             </ul>
+//                         )}
+//                     </li>
+//                 ))}
+//             </ul>
+//             <Link to="/NewTask">New Task</Link>
+//         </div>
+//     );
+// };
+
+// export default TaskLists;
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
@@ -40,7 +195,7 @@ const TaskLists = () => {
     const fetchCheckListItems = async (tasksId: number) => {
         try {
             const response = await axios.get(
-                `http://localhost:3000/checkListItems?tasksId=${tasksId}`,
+                `http://localhost:3000/checkListItems/${tasksId}`,
                 {
                     headers: {
                         Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
@@ -104,50 +259,65 @@ const TaskLists = () => {
         setTasks(updatedTasks);
     };
 
-    useEffect(() => {
-        tasks.forEach((task) => {
-            if (!task.checkListItems) {
-                fetchCheckListItems(task.id);
-            }
-        });
-    }, [tasks]);
+    const handleRemoveCheckListItem = async (checkListItemId: number) => {
+        try {
+            await axios.delete(`http://localhost:3000/checkListItems/${checkListItemId}`, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+                },
+            });
+
+            const updatedTasks = tasks.map((task) => {
+                if (task.checkListItems) {
+                    const updatedCheckListItems = task.checkListItems.filter(
+                        (item) => item.id !== checkListItemId
+                    );
+                    return { ...task, checkListItems: updatedCheckListItems };
+                }
+                return task;
+            });
+
+            setTasks(updatedTasks);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     return (
         <div>
-            <h1>Task List</h1>
-            <ul>
-                {tasks.map((task) => (
-                    <li key={task.id}>
-                        <input
-                            type="checkbox"
-                            checked={task.done}
-                            onChange={() => handleDone(task.id)}
-                        />
-                        <span>{task.designation}</span>
-                        <button onClick={() => handleDelete(task.id)}>Delete</button>
-                        <button onClick={() => handleAddCheckListItem(task.id)}>
-                            Add Check List Item
-                        </button>
-                        {task.checkListItems && (
-                            <ul>
-                                {task.checkListItems.map((checkListItem) => (
-                                    <li key={checkListItem.id}>
-                                        {checkListItem.name}
-                                        <button
-                                            onClick={() =>
-                                                handleDoneCheckListItem(task.id, checkListItem.id)
-                                            }
-                                        >
-                                            Done
-                                        </button>
-                                    </li>
-                                ))}
-                            </ul>
-                        )}
-                    </li>
-                ))}
-            </ul>
-            <Link to="/NewTask">New Task</Link>
+            <h2>Task Lists</h2>
+            {tasks.map((task) => (
+                <div key={task.id}>
+                    <h3>{task.designation}</h3>
+                    <p>{task.deadline}</p>
+                    <p>{task.notes}</p>
+                    <input
+                        type="checkbox"
+                        checked={task.done}
+                        onChange={() => handleDone(task.id)}
+                    />
+                    <button onClick={() => handleDelete(task.id)}>Delete</button>
+                    <button onClick={() => fetchCheckListItems(task.id)}>Show CheckList</button>
+                    {task.checkListItems &&
+                        task.checkListItems.map((item) => (
+                            <div key={item.id}>
+                                <p>{item.name}</p>
+                                <input
+                                    type="checkbox"
+                                    onChange={() => handleDoneCheckListItem(task.id, item.id)}
+                                />
+                                <button onClick={() => handleRemoveCheckListItem(item.id)}>
+                                    Remove
+                                </button>
+                            </div>
+                        ))}
+                    <button onClick={() => handleAddCheckListItem(task.id)}>
+                        Add CheckList Item
+                    </button>
+                    <hr />
+                </div>
+            ))}
+            <Link to="/NewTask">Create Task</Link>
         </div>
     );
 };
