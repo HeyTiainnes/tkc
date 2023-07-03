a8a470076f95dee5194e4681844cadf9989910f3
HeyTiainnes <etienne.pomente@reseau.sncf.fr>
2023-07-03 21:33:11 +0200

    start responsivity

--- src/pages/TaskLists.tsx
+++ src/pages/TaskLists.tsx
@@ -156,7 +156,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-
+import './TaskLists.css';
 export type Task = {
     id: number;
     designation: string;
