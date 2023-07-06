2a279c69d2dcb87a36c215c5bc7f1a99aee93178
HeyTiainnes <etienne.pomente@reseau.sncf.fr>
2023-07-06 21:42:59 +0200

    menage

--- src/pages/NewCheckList.tsx
+++ src/pages/NewCheckList.tsx
@@ -65,5 +65,177 @@ const NewCheckList = () => {
         </div>
     );
 };
+export default NewCheckList
+// export default NewCheckList;
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import { useParams, useNavigate } from 'react-router-dom';
 
-export default NewCheckList;
+// interface RouteParams {
+//     taskId: string;
+//     checkListItemId?: string;
+// }
+
+// const NewCheckList = () => {
+//     const [name, setName] = useState('');
+//     const [notes, setNotes] = useState('');
+//     const navigate = useNavigate();
+//     const { taskId, checkListItemId } = useParams<RouteParams>();
+
+//     useEffect(() => {
+//         const fetchCheckListItem = async () => {
+//             try {
+//                 const response = await axios.get(
+//                     `http://localhost:3000/checkListItems/${checkListItemId}`,
+//                     {
+//                         headers: {
+//                             Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+//                         },
+//                     }
+//                 );
+
+//                 const checkListItemData = response.data;
+//                 setName(checkListItemData.name);
+//                 setNotes(checkListItemData.notes);
+//             } catch (error) {
+//                 console.error(error);
+//             }
+//         };
+
+//         if (checkListItemId) {
+//             fetchCheckListItem();
+//         }
+//     }, [checkListItemId]);
+
+//     const handleSubmit = async (e: React.FormEvent) => {
+//         e.preventDefault();
+
+//         try {
+//             const taskIdParsed = parseInt(taskId, 10);
+//             const data = {
+//                 name,
+//                 notes,
+//                 tasksId: taskIdParsed,
+//             };
+
+//             if (checkListItemId) {
+//                 await axios.put(
+//                     `http://localhost:3000/checkListItems/${checkListItemId}`,
+//                     data,
+//                     {
+//                         headers: {
+//                             Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+//                         },
+//                     }
+//                 );
+//             } else {
+//                 await axios.post('http://localhost:3000/checkListItems', data, {
+//                     headers: {
+//                         Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+//                     },
+//                 });
+//             }
+
+//             navigate(`/TaskLists`);
+//         } catch (error) {
+//             console.error(error);
+//         }
+//     };
+
+//     return (
+//         <div>
+//             <form onSubmit={handleSubmit}>
+//                 <label htmlFor="name">Nom de la sous-tâche:</label>
+//                 <input
+//                     type="text"
+//                     id="name"
+//                     value={name}
+//                     onChange={(e) => setName(e.target.value)}
+//                 />
+
+//                 <label htmlFor="notes">Notes (max. 1024 caractères):</label>
+//                 <textarea
+//                     id="notes"
+//                     value={notes}
+//                     onChange={(e) => setNotes(e.target.value)}
+//                 />
+
+//                 <button type="submit">Enregistrer</button>
+//                 <button type="button" onClick={() => navigate('/TaskLists')}>
+//                     Annuler
+//                 </button>
+//             </form>
+//         </div>
+//     );
+// };
+
+// export default NewCheckList;
+//  import { useState } from "react";
+// import axios from "axios";
+// import { useState } from "react";
+// import { useParams, useNavigate } from "react-router-dom";
+
+// interface RouteParams {
+//     taskId: string;
+//     [key: string]: string | undefined;
+// }
+
+// const NewCheckList = () => {
+//     const [name, setName] = useState("");
+//     const [notes, setNotes] = useState("");
+//     const navigate = useNavigate();
+//     const { taskId } = useParams<RouteParams>();
+
+//     const handleSubmit = async (e: React.FormEvent) => {
+//         e.preventDefault();
+
+//         try {
+//             const taskIdParsed = taskId ? parseInt(taskId, 10) : 0;
+//             const response = await axios.post("http://localhost:3000/checkListItems", {
+//                 name,
+//                 notes,
+//                 tasksId: taskIdParsed, // Utilisation de "tasksId" au lieu de "taskId"
+//             });
+
+//             setName("");
+//             setNotes("");
+
+//             console.log(response.data);
+
+//             navigate(`/TaskLists`); // Redirection vers la page TaskLists
+//         } catch (error) {
+//             console.error(error);
+//         }
+//     };
+
+//     return (
+//         <div>
+//             <form onSubmit={handleSubmit}>
+//                 <label htmlFor="name">Nom de la sous-tâche:</label>
+//                 <input
+//                     type="text"
+//                     id="name"
+//                     value={name}
+//                     onChange={(e) => setName(e.target.value)}
+//                 />
+
+//                 <label htmlFor="notes">Notes (max. 1024 caractères):</label>
+//                 <textarea
+//                     id="notes"
+//                     value={notes}
+//                     onChange={(e) => setNotes(e.target.value)}
+//                 />
+
+//                 <button type="submit">Enregistrer</button>
+//                 <button
+//                     type="button"
+//                     onClick={() => navigate(`/TaskLists`)} // Redirection vers la page TaskLists
+//                 >
+//                     Annuler
+//                 </button>
+//             </form>
+//         </div>
+//     );
+// };
+
+// export default NewCheckList;
--- src/pages/NewTask.tsx
+++ src/pages/NewTask.tsx
@@ -1,3 +1,84 @@
+// // import React, { FormEvent, useRef, useState } from "react";
+// import axios from "axios";
+// import { useNavigate } from "react-router-dom";
+// import "./NewTask.css";
+// import { FormEvent, useRef, useState } from "react";
+
+// type NewTaskProps = {
+//     redirectTo: string;
+// };
+
+// const NewTask = ({ redirectTo }: NewTaskProps) => {
+//     const designationRef = useRef<HTMLInputElement>(null);
+//     const deadlineRef = useRef<HTMLInputElement>(null);
+//     const [designation, setDesignation] = useState<string>("");
+//     const [deadline, setDeadline] = useState<string>("");
+//     const navigate = useNavigate();
+
+//     const handleSubmit = (e: FormEvent) => {
+//         e.preventDefault();
+//         const data = {
+//             designation,
+//             deadline,
+//         };
+
+//         axios
+//             .post("http://localhost:3000/tasks", data, {
+//                 headers: {
+//                     Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+//                 },
+//             })
+//             .then(() => {
+//                 navigate(redirectTo);
+//             })
+//             .catch((error) => {
+//                 console.error("Erreur lors de l'envoi :", error);
+//             });
+//     };
+
+//     return (
+//         <div className="container">
+//             <form onSubmit={handleSubmit}>
+//                 <label htmlFor="designation">
+//                     Designation<span className="required">*</span>
+//                 </label>
+//                 <br />
+//                 <input
+//                     type="text"
+//                     id="designation"
+//                     name="designation"
+//                     required
+//                     ref={designationRef}
+//                     value={designation}
+//                     onChange={(e) => setDesignation(e.target.value)}
+//                 />
+//                 <br />
+//                 <label htmlFor="deadline">Deadline</label>
+//                 <br />
+//                 <input
+//                     type="date"
+//                     id="deadline"
+//                     name="deadline"
+//                     value={deadline}
+//                     onChange={(e) => setDeadline(e.target.value)}
+//                     ref={deadlineRef}
+//                 />
+//                 <br />
+
+//                 <div className="buttons">
+//                     <button type="reset" id="cancel">
+//                         Cancel
+//                     </button>
+//                     <button type="submit" id="submit">
+//                         Validate
+//                     </button>
+//                 </div>
+//             </form>
+//         </div>
+//     );
+// };
+
+// export default NewTask;
 // import React, { FormEvent, useRef, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
@@ -77,5 +158,4 @@ const NewTask = ({ redirectTo }: NewTaskProps) => {
         </div>
     );
 };
-
 export default NewTask;
