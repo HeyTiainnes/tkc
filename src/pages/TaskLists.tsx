daba9cd1fad4547d01eff1f91d1b88393fcd1f9c
HeyTiainnes <etienne.pomente@reseau.sncf.fr>
2023-07-10 00:22:22 +0200

    dates ok

--- src/pages/TaskLists.tsx
+++ src/pages/TaskLists.tsx
@@ -1,218 +1,4 @@
-// import React, { useState, useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
-// import axios from "axios";
-// import "./tasksList.css";
 
-// export type Task = {
-//     id: number;
-//     designation: string;
-//     deadline: string;
-//     notes: string;
-//     done: boolean;
-//     checkListItems?: CheckListItem[];
-// };
-
-// export type CheckListItem = {
-//     id: number;
-//     name: string;
-// };
-
-// const TaskLists = () => {
-//     const [tasks, setTasks] = useState<Task[]>([]);
-//     const [editingTaskId, setEditingTaskId] = useState<number | null>(null);
-//     const [editedTasks, setEditedTasks] = useState<Record<number, Partial<Task>>>({});
-//     const navigate = useNavigate();
-
-//     useEffect(() => {
-//         const fetchTasks = async () => {
-//             try {
-//                 const response = await axios.get("http://localhost:3000/tasks", {
-//                     headers: {
-//                         Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-//                     },
-//                 });
-//                 setTasks(response.data);
-//             } catch (error) {
-//                 console.error(error);
-//             }
-//         };
-
-//         fetchTasks();
-//     }, []);
-
-//     const handleDone = (taskId: number) => {
-//         const updatedTasks = tasks.map((task) => {
-//             if (task.id === taskId) {
-//                 return { ...task, done: !task.done };
-//             }
-//             return task;
-//         });
-
-//         setTasks(updatedTasks);
-//     };
-
-//     const handleDelete = (taskId: number) => {
-//         axios
-//             .delete(`http://localhost:3000/tasks/${taskId}`, {
-//                 headers: {
-//                     Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-//                 },
-//             })
-//             .then(() => {
-//                 const updatedTasks = tasks.filter((task) => task.id !== taskId);
-//                 setTasks(updatedTasks);
-//             })
-//             .catch((error) => {
-//                 console.error(error);
-//             });
-//     };
-
-//     const handleAddCheckListItem = (taskId: number) => {
-//         navigate(`/NewCheckList/${taskId}`);
-//     };
-
-//     const handleRemoveCheckListItem = async (checkListItemId: number) => {
-//         try {
-//             await axios.delete(`http://localhost:3000/checkListItems/${checkListItemId}`, {
-//                 headers: {
-//                     Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-//                 },
-//             });
-
-//             const updatedTasks = tasks.map((task) => {
-//                 if (task.checkListItems) {
-//                     const updatedCheckListItems = task.checkListItems.filter((item) => item.id !== checkListItemId);
-//                     return { ...task, checkListItems: updatedCheckListItems };
-//                 }
-//                 return task;
-//             });
-
-//             setTasks(updatedTasks);
-//         } catch (error) {
-//             console.error(error);
-//         }
-//     };
-
-//     const handleEdit = (taskId: number) => {
-//         const taskToEdit = tasks.find((task) => task.id === taskId);
-//         if (taskToEdit) {
-//             setEditedTasks((prevState) => ({
-//                 ...prevState,
-//                 [taskId]: {
-//                     designation: taskToEdit.designation,
-//                     deadline: taskToEdit.deadline,
-//                     notes: taskToEdit.notes,
-//                 },
-//             }));
-//             setEditingTaskId(taskId);
-//         }
-//     };
-
-//     const handleSave = (taskId: number) => {
-//         const updatedTasks = tasks.map((task) => {
-//             if (task.id === taskId) {
-//                 return {
-//                     ...task,
-//                     designation: editedTasks[taskId]?.designation || task.designation,
-//                     deadline: editedTasks[taskId]?.deadline || task.deadline,
-//                     notes: editedTasks[taskId]?.notes || task.notes,
-//                 };
-//             }
-//             return task;
-//         });
-
-//         setTasks(updatedTasks);
-//         setEditingTaskId(null);
-//     };
-
-//     const handleCancel = () => {
-//         setEditingTaskId(null);
-//     };
-
-//     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-//         const { name, value } = e.target;
-//         setEditedTasks((prevState) => ({
-//             ...prevState,
-//             [editingTaskId!]: {
-//                 ...prevState[editingTaskId!],
-//                 [name]: value,
-//             },
-//         }));
-//     };
-
-//     const handleDeleteTask = async (taskId: number) => {
-//         try {
-//             await axios.delete(`http://localhost:3000/tasks/${taskId}`, {
-//                 headers: {
-//                     Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-//                 },
-//             });
-
-//             const updatedTasks = tasks.filter((task) => task.id !== taskId);
-//             setTasks(updatedTasks);
-//         } catch (error) {
-//             console.error(error);
-//         }
-//     };
-
-//     return (
-//         <div>
-//             <h5>Liste des tâches et sous-tâches prévues</h5>
-//             {tasks.map((task) => (
-//                 <div key={task.id}>
-//                     {editingTaskId === task.id ? (
-//                         <>
-//                             <label htmlFor="designation">Designation</label>
-//                             <input
-//                                 type="text"
-//                                 name="designation"
-//                                 value={editedTasks[task.id]?.designation || task.designation}
-//                                 onChange={handleInputChange}
-//                             />
-//                             <br />
-//                             <label htmlFor="deadline">Deadline</label>
-//                             <input
-//                                 type="date"
-//                                 name="deadline"
-//                                 value={editedTasks[task.id]?.deadline || task.deadline}
-//                                 onChange={handleInputChange}
-//                             />
-//                             <br />
-//                             <label htmlFor="notes">Notes</label>
-//                             <textarea
-//                                 name="notes"
-//                                 value={editedTasks[task.id]?.notes || task.notes}
-//                                 onChange={handleInputChange}
-//                             />
-//                             <br />
-//                             <button onClick={() => handleSave(task.id)}>Valider</button>
-//                             <button onClick={handleCancel}>Annuler</button>
-//                         </>
-//                     ) : (
-//                         <>
-//                             <h3>{task.designation}</h3>
-//                             <p>{task.deadline}</p>
-//                             <p>{task.notes}</p>
-//                             <button onClick={() => handleDeleteTask(task.id)}>Supprimer</button> {/* Bouton Supprimer */}
-//                             {task.checkListItems &&
-//                                 task.checkListItems.map((item) => (
-//                                     <div key={item.id}>
-//                                         <p>{item.name}</p>
-//                                         <button onClick={() => handleRemoveCheckListItem(item.id)}>Supprimer</button>
-//                                     </div>
-//                                 ))}
-//                             <button onClick={() => handleAddCheckListItem(task.id)}>Ajouter une sous-tâche</button>
-//                             <button onClick={() => handleEdit(task.id)}>Modifier</button>
-//                         </>
-//                     )}
-//                     <hr />
-//                 </div>
-//             ))}
-//         </div>
-//     );
-// };
-
-// export default TaskLists;
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
@@ -232,7 +18,7 @@ export type CheckListItem = {
     name: string;
 };
 
-const TaskLists = () => {
+const TaskList = () => {
     const [tasks, setTasks] = useState<Task[]>([]);
     const [editingTaskId, setEditingTaskId] = useState<number | null>(null);
     const [editedTasks, setEditedTasks] = useState<Record<number, Partial<Task>>>({});
@@ -390,7 +176,7 @@ const TaskLists = () => {
                                 <input
                                     type="date"
                                     name="deadline"
-                                    value={editedTasks[task.id]?.deadline || task.deadline}
+                                    value={editedTasks[task.id]?.deadline || task.deadline || ""}
                                     onChange={handleInputChange}
                                 />
                                 <br />
@@ -410,12 +196,13 @@ const TaskLists = () => {
                             </>
                         ) : (
                             <>
+                                ```jsx
                                 <h3>{task.designation}</h3>
                                 <p>{task.deadline}</p>
                                 <p>{task.notes}</p>
                                 <button className="delete-button" onClick={() => handleDeleteTask(task.id)}>
                                     Supprimer
-                                </button> {/* Bouton Supprimer */}
+                                </button>
                                 {task.checkListItems &&
                                     task.checkListItems.map((item) => (
                                         <div className="subtask" key={item.id}>
@@ -440,4 +227,4 @@ const TaskLists = () => {
     );
 };
 
-export default TaskLists;
+export default TaskList;
